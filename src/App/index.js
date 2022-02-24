13b2647b043211359bb4b81397c85f1681017968
Ivis Orea <ivisorea@gmail.com>
2022-02-24 15:20:24 +0100

    ADD Context

--- src/App/index.js
+++ src/App/index.js
@@ -1,58 +1,13 @@
-import React, { useState} from 'react';
+import React from 'react';
+import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-
-  const [item, setItem] = useState(!localStorageItem ? JSON.stringify(initialValue) : JSON.parse(localStorageItem));
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  }
-  return [item, saveItem];
-}
-
 export const App = () => {
-  const [todos, saveTodos] = useLocalStorage('TODOS', []);
-  const [search, setSearch] = useState('')
   
-  const completedTodos = todos.filter(todo => !!todo.isCompleted).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-  if (!search.length >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter(todo => {
-      return todo.text.toLowerCase().includes(search.toLowerCase());
-      });
-
-  }
-
-  const completeTodos = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos[todoIndex].isCompleted = true;
-    saveTodos(newTodos);
-  }
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  }
-
   return (
-      <AppUI
-        totalTodos={totalTodos}
-        completedTodos={completedTodos}
-        search={search}
-        setSearch={setSearch}
-        searchedTodos={searchedTodos}
-        completeTodos={completeTodos}
-        deleteTodo={deleteTodo}
-      />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+    
   )
 }
